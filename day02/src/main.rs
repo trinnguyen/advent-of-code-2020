51594b96380cd0589d02bcf4638e9377ae9f256e
Tri Nguyen <tnn@trinnguyen.com>
2020-12-02 18:38:06 +0100

    finished part2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     let mut str = String::new();
     std::io::stdin().read_to_string(&mut str).unwrap();
     
+    calc_2(str.lines());
     calc_1(str.lines());
 }
 
@@ -40,16 +41,36 @@ fn calc_1(lines: std::str::Lines) -> () {
 
     println!("result: {}", global_cnt);
 }
- 
 
-fn parse_line(line: &str) -> (u32, u32, char, &str) {
+fn calc_2(lines: std::str::Lines) -> () {
+    let mut global_cnt = 0;
+
+    for line in lines {
+        // fmt: 1-3 a: abcde
+        let (fst, snd, ch, pwd) = parse_line(line);
+        if fst == 0 || snd == 0 {
+            continue;
+        }
+
+        let bytes = pwd.as_bytes();
+        let is_fst_matched = bytes[fst - 1] == ch as u8;
+        let is_snd_matched = bytes[snd - 1] == ch as u8;
+        if (is_fst_matched || is_snd_matched) && !(is_fst_matched && is_snd_matched) {
+            global_cnt = global_cnt + 1;
+        }
+    }
+
+    println!("result: {}", global_cnt);
+}
+
+fn parse_line(line: &str) -> (usize, usize, char, &str) {
     // 1-3 a: abcde
     let items: Vec<_> = line.split(' ').collect();
 
     // range
     let range: Vec<_> = items.get(0).unwrap().split('-').collect();
-    let min: u32 = range.get(0).unwrap().parse().unwrap();
-    let max: u32 = range.get(1).unwrap().parse().unwrap();
+    let min: usize = range.get(0).unwrap().parse().unwrap();
+    let max: usize = range.get(1).unwrap().parse().unwrap();
 
     // char
     let ch = items.get(1).unwrap().chars().next().unwrap();
