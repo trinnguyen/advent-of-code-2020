5472c510715fa334536f5210969a6c4af4ffd2ec
Tri Nguyen <tnn@trinnguyen.com>
2020-12-09 16:51:04 +0100

    day06 part2

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -7,26 +7,36 @@ fn main() {
     std::io::stdin().read_to_string(&mut str).unwrap();
 
     println!("result part-1: {:?}", part_1(str.as_str()));
-    // println!("result part-2: {:?}", part_2(str.as_str()));
+    println!("result part-2: {:?}", part_2(str.as_str()));
 }
 
 fn part_1(str: &str) -> usize {
     let mut sum = 0;
     let mut lines = str.lines();
     loop {
-        if let Some(gr) = next_group(&mut lines) {
-            let len = gr.len();
-            println!("new group: {}", len);
-            sum += len;
-        } else {
-            break;
+        match next_group_disjoint(&mut lines) {
+            Some(gr) => sum += gr.len(),
+            _ => break
         }
     }
 
     return sum;
 }
 
-fn next_group(line: &mut Lines) -> Option<HashSet<char>> {
+fn part_2(str: &str) -> usize {
+    let mut sum = 0;
+    let mut lines = str.lines();
+    loop {
+        match next_group_common(&mut lines) {
+            Some(gr) => sum += gr.len(),
+            _ => break
+        }
+    }
+
+    return sum;
+}
+
+fn next_group_disjoint(line: &mut Lines) -> Option<HashSet<char>> {
     return match line.next() {
         Some(str) => {
             let mut answers = HashSet::new();
@@ -35,7 +45,6 @@ fn next_group(line: &mut Lines) -> Option<HashSet<char>> {
 
                 // empty line -> end of group
                 if val.is_empty() {
-                    println!("{:?}", answers);
                     return Some(answers)
                 }
 
@@ -52,6 +61,50 @@ fn next_group(line: &mut Lines) -> Option<HashSet<char>> {
                     _ => val = "" // EOF
                 }
             }
+        },
+        _ => None
+    }
+}
+
+fn next_group_common(line: &mut Lines) -> Option<HashSet<char>> {
+    return match line.next() {
+        Some(str) => {
+            let mut answers = HashSet::new();
+
+            // add first line
+            str.chars()
+                .filter(|c| matches!(c, 'a'..='z'))
+                .for_each(|c| {
+                    let _ = answers.insert(c);
+                });
+
+            // next line
+            loop {
+                match line.next() {
+                    Some(str2) => {
+
+                        // empty line -> end of group
+                        if str2.is_empty() {
+                            break
+                        }
+
+                        // find common
+                        let common: Vec<char> = str2.chars()
+                            .filter(|c| {
+                                matches!(c, 'a'..='z') && answers.contains(c)
+                            }).collect();
+
+                        // update existing
+                        answers.clear();
+                        common.iter().for_each(|c| {
+                            let _ = answers.insert(*c);
+                        });
+                    },
+                    _ => break
+                }
+            }
+
+            return Some(answers)
         },
         _ => None
     }
