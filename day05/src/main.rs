d112fd514215bcd31e4b3f8114ab35a664ac6231
Tri Nguyen <tnn@trinnguyen.com>
2020-12-09 14:20:21 +0100

    day05 part1

---
+++ day05/src/main.rs
@@ -0,0 +1,81 @@
+use std::io::Read;
+
+fn main() {
+    let mut str = String::new();
+    std::io::stdin().read_to_string(&mut str).unwrap();
+    
+    println!("result part-1: {:?}", part_1(str.as_str()));
+}
+
+fn part_1(str: &str) -> u32 {
+    let mut max: u32 = 0;
+    for line in str.lines() {
+        let id = get_id(line);
+        if id > max {
+            max = id;
+        }
+    }
+
+    return max
+}
+
+fn get_id(str: &str) -> u32 {
+    let seat = get_seat(str);
+    return seat.0 * 8 + seat.1;
+}
+
+fn get_seat(str: &str) -> (u32, u32) {
+    if str.len() < 10 {
+        panic!("invalid line, expect at least 10 chars")
+    }
+
+    let bytes = str.as_bytes();
+
+    // read col
+    let col = get_value_in_range(&bytes[0..=6], 0, 127, b'F', b'B');
+    let row = get_value_in_range(&bytes[7..=9], 0, 7, b'L', b'R');
+    return (col, row)
+}
+
+fn get_value_in_range(bytes: &[u8], min: u32, max: u32, lower: u8, upper: u8) -> u32 {
+    let mut range = (min, max);
+    let len = bytes.len();
+    for i in 0..len {
+        let mid: u32 = range.0 + (range.1 - range.0 + 1) / 2;
+        let b = bytes[i];
+        range = match b {
+            b if b == lower => (range.0, mid - 1),
+            b if b == upper => (mid, range.1),
+            _ => panic!("unexpected char: {}", b)
+        }
+    }
+
+    // final
+    let col = match bytes[len-1] {
+        b if b == lower => range.0,
+        b if b == upper => range.1,
+        _ => panic!("unexpected char", )
+    };
+
+    return col;
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{get_id, get_seat};
+
+    #[test]
+    fn test_get_seat() {
+        assert_eq!((44, 5), get_seat("FBFBBFFRLR"));
+        assert_eq!((70, 7), get_seat("BFFFBBFRRR"));
+        assert_eq!((14, 7), get_seat("FFFBBBFRRR"));
+        assert_eq!((102, 4), get_seat("BBFFBBFRLL"));
+    }
+
+    #[test]
+    fn test_get_id() {
+        assert_eq!(567, get_id("BFFFBBFRRR"));
+        assert_eq!(119, get_id("FFFBBBFRRR"));
+        assert_eq!(820, get_id("BBFFBBFRLL"));
+    }
+}
