1d6b99c1dab0044f3b12914695f150ea3185bb26
Tri Nguyen <tri@trinnguyen.com>
2020-12-15 21:51:11 +0100

    day15 part2

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -5,21 +5,27 @@ fn main() {
     let mut str = String::new();
     std::io::stdin().read_to_string(&mut str).unwrap();
     println!("result part-1: {:?}", part_1(&str));
+    println!("result part-2: {:?}", part_2(&str));
 }
 
 fn part_1(str: &str) -> u32 {
     let vec: Vec<u32> = str.split(',').map(|c| c.parse::<u32>().unwrap()).collect();
-    calc_part_1(&vec)
+    calc_number(&vec, 2020)
 }
 
-fn calc_part_1(vec: &Vec<u32>) -> u32 {
+fn part_2(str: &str) -> u32 {
+    let vec: Vec<u32> = str.split(',').map(|c| c.parse::<u32>().unwrap()).collect();
+    calc_number(&vec, 30000000)
+}
+
+fn calc_number(vec: &Vec<u32>, pos: usize) -> u32 {
     let mut last_num = 0;
     let mut turn: usize = 0;
     let mut map: HashMap<u32, (usize, usize)> = HashMap::new();
 
-    while turn < 2020 {
+    while turn < pos {
         turn = turn + 1;
-        
+
         last_num = match vec.get(turn - 1) {
             Some(v) => *v,
             None => {
@@ -53,13 +59,13 @@ fn calc_part_1(vec: &Vec<u32>) -> u32 {
 
 #[cfg(test)]
 mod tests {
-    use crate::calc_part_1;
+    use crate::calc_number;
 
     #[test]
     fn test_part_1() {
-        assert_eq!(436, calc_part_1(&vec![0, 3, 6]));
-        assert_eq!(1, calc_part_1(&vec![1,3,2]));
-        assert_eq!(10, calc_part_1(&vec![2,1,3]));
-        assert_eq!(27, calc_part_1(&vec![1,2,3]));
+        assert_eq!(436, calc_number(&vec![0, 3, 6], 2020));
+        assert_eq!(1, calc_number(&vec![1,3,2], 2020));
+        assert_eq!(10, calc_number(&vec![2,1,3], 2020));
+        assert_eq!(27, calc_number(&vec![1,2,3], 2020));
     }
 }
